b44c36645aeddffc8dc96f5179de945a64e9740f
danujaysl <COHDSE201F-061@student.nibm.lk>
2023-09-09 21:04:42 +0530

    added changes

--- js/script.js
+++ js/script.js
@@ -8,30 +8,31 @@ function fnReg() {
 
   if (js_password != js_repassword) {
     alert("Re-typed password is not match");
+  } else if (js_age < 18) {
+    alert("Sorry ! User should be older than 18!");
+  } else {
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:8080/api/users/saveJS",
+      data: JSON.stringify({
+        n_jobseekerid: "0",
+        c_name: js_name,
+        c_logname: js_username,
+        c_password: js_password,
+        n_age: js_age,
+        c_telephone: js_tele,
+      }),
+      contentType: "application/json; charset=utf-8", // this
+      dataType: "json", // and this
+      success: function (response) {
+        alert(response.message);
+      },
+      error: function (response) {
+        alert(response.message);
+      },
+    });
   }
-
-  $.ajax({
-    type: "POST",
-    url: "http://localhost:8080/api/users/saveJS",
-    data: JSON.stringify({
-      n_jobseekerid: "0",
-      c_name: js_name,
-      c_logname: js_username,
-      c_password: js_password,
-      n_age: js_age,
-      c_telephone: js_tele,
-    }),
-    contentType: "application/json; charset=utf-8", // this
-    dataType: "json", // and this
-    success: function (response) {
-      alert(response.message);
-    },
-    error: function (response) {
-      alert(response.message);
-    },
-  });
 }
-
 // LOGIN FROM HERE
 function fncLogin() {
   let type = $("#styledCombo").val();
@@ -55,6 +56,8 @@ function fncLogin() {
         } else if (response.message == "101") {
           alert("Password is incorrect!");
         } else if (response.message == "100") {
+          const userObj = response.content
+          localStorage.setItem('userId', userObj.n_jobseekerid);
           location.href = "./dashboard.html";
         } else {
           alert("Login error! Please contact Administrator!");
@@ -81,6 +84,7 @@ function fncLogin() {
         } else if (response.message == "101") {
           alert("Password is incorrect!");
         } else if (response.message == "200") {
+          // localStorage.setItem('userId', userObj.n_jobseekerid); //set as consultant id
           location.href = "./dashboardAgent.html";
         } else {
           alert("Login error! Please contact Administrator!");
@@ -93,10 +97,10 @@ function fncLogin() {
   } else if (type == 3) {
     $.ajax({
       type: "POST",
-      url: "http://localhost:8080/api/users/login", // API SHOULD BE CHANGED
+      url: "http://localhost:8080/api/admin/login",
       data: JSON.stringify({
         type: type,
-        c_logname: username,
+        c_name: username,
         c_password: password,
       }),
       contentType: "application/json; charset=utf-8", // this
@@ -118,3 +122,29 @@ function fncLogin() {
     });
   }
 }
+
+function fncSelectAgent() {
+
+  let jobtype = $("#refjobtype").val();
+
+  if (jobtype=null){
+
+  }
+
+  fetch("/combo/selectAgent")
+    .then((response) => response.json())
+    .then((data) => {
+      // Populate the combo box with data
+      const comboBox = document.getElementById("myComboBox");
+      data.forEach((item) => {
+        const option = document.createElement("option");
+        option.value = item.id; // Set the value to match the data from the database
+        option.textContent = item.name; // Set the text to display in the combo box
+        comboBox.appendChild(option);
+      });
+    })
+    .catch((error) => console.error("Error:", error));
+}
+
+
+
--- js/scriptdash.js
+++ js/scriptdash.js
@@ -3,3 +3,44 @@ function fncLogout() {
        location.href="./login.html";
     }
   }
+
+  function loadConsultants(){
+   const country = document.getElementById('refcountry');
+   const job = document.getElementById('refjobtype');
+ 
+   if(country.value !== "" &&  job.value !== ""){
+     fetch("http://127.0.0.1:8080/api/consultant/get-consultatnts-by-availability", {
+      method:"POST",
+      headers:{
+         'Content-type':'application/json'
+      },
+      body:JSON.stringify({
+         "n_country": country.value,
+         "n_jobid": job.value
+      })
+     })
+     .then((response) => response.json())
+     .then((data) => {
+       const consultantsComboBox = document.getElementById('refconsultant');
+       const consultants = data?.content;
+       const optionsArray = consultants.map(consultant=>`
+       <option value="${consultant.n_consultantid}">${consultant.c_name}</option>
+       `)
+       consultantsComboBox.innerHTML = optionsArray.join('');
+ 
+ 
+     })
+     .catch((error) => console.error("Error:", error));
+ 
+ 
+   }
+ 
+ }
+
+
+ function placeAppointment(){
+   const country = document.getElementById('refcountry');
+   const job = document.getElementById('refjobtype');
+   const consultantsComboBox = document.getElementById('refconsultant');
+   
+ }
